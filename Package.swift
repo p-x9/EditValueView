8b85deadb79158b76a1b54b42c9531216631b600
p-x9 <50244599+p-x9@users.noreply.github.com>
2024-03-31 02:43:36 +0900

    bump `ReflectionView` to 0.8.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/p-x9/SwiftUIColor.git", .upToNextMajor(from: "0.3.0")),
         .package(url: "https://github.com/p-x9/swift-magic-mirror.git", from: "0.2.0"),
-        .package(url: "https://github.com/p-x9/swiftui-reflection-view.git", from: "0.7.0")
+        .package(url: "https://github.com/p-x9/swiftui-reflection-view.git", from: "0.8.0")
     ],
     targets: [
         .target(
